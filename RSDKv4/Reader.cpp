b1ddd7343f7ba903f3fd392e38cfd8340a6012c0
DropSonic <130788813+DropSonic0@users.noreply.github.com>
2025-07-18 17:03:18 -0300

    new

--- RSDKv4/Reader.cpp
+++ RSDKv4/Reader.cpp
@@ -4,7 +4,7 @@
 RSDKContainer rsdkContainer;
 
 char fileName[0x100];
-byte fileBuffer[0x2000];
+byte *fileBuffer      = NULL;
 int fileSize          = 0;
 int vFileSize         = 0;
 int readPos           = 0;
